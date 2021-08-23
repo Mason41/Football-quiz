f317e2196f55e0320c187510c8b9693415758c78
Mason41 <masonweeks42@gmail.com>
2021-08-22 20:35:37 -0600

    adding in java script scores and questions

---
+++ assets/js/questions.js
@@ -0,0 +1,32 @@
+var questions = [
+    {
+        title: "What quarter back has won the most super bowls?",
+        choices: ["Tom Brady", "Joe Montana", "Terry Bradshaw", "Troy Aikman"],
+        answer: "Tom Brady"
+    },
+    {
+        title: "What quarter back has thrown the most yards in a season?",
+        choices: ["Jameis Winston", "Ben Roethlisberger", "Patrick Mahomes", "Drew Brees"],
+        answer: "Drew Brees"
+    },
+    {
+        title: "What quarterbacks have thrown for over 500 carrer touchdown passes?",
+        choices: ["Peyton Manning", "Brett Favre", "Drew Brees", "Tom Brady", "All the above"],
+        answer: "All the above"
+    },
+    {
+        title: "What wide receiver has the most single season receiving yards:",
+        choices: ["Julio Jones", "Jerry Rice", "Calvin Johnson", "Antonio brown"],
+        answer: "Calvin Johnson"
+    },
+    {
+        title: "What running back has over 2,000 rushing yards in a single season",
+        choices: ["Eric Dickerson", "Chris Johnson", "Adrian Peterson", "Barry Sanders", "All the above"],
+        answer: "All the above"
+    },
+    {
+        title: "What Receiver has the most carrer receiving yards",
+        choices: ["Larry Fitzgerald", "Jerry Rice", "Terrell Owens", "Randy Moss"],
+        answer: "Jerry Rice"
+    }
+];
